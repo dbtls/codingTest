0677e8214eb206a31c84508434aa07904970d418
yushinKim <dbtls0218@naver.com>
2026-08-25 23:27:05 +0900

    8/25

--- src/chap1/c1_4.java
+++ src/chap1/c1_4.java
@@ -28,5 +28,11 @@ giB
 import java.io.*;
 public class c1_4 {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine().trim());
+        for(int i=0;i<n;i++){
+            StringBuilder sb = new StringBuilder(br.readLine());
+            System.out.println(sb.reverse());
+        }
     }
 }
